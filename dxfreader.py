4e65dceb472bc95f9644439f2dff67e5b1f6be52
CompetitiveSplit <narvaez0526@gmail.com>
2023-10-10 03:04:37 -0500

    Obtiene las coordenadas y longitud de linea

--- dxfreader.py
+++ dxfreader.py
@@ -5,20 +5,19 @@ import math
 try:
     my_file = ezdxf.readfile("testreader.dxf")
     msp = my_file.modelspace()
-
-    def longitude(start, end):
+    total_length = 0
+    def get_lenght(start, end):
         return (math.dist(start, end), 2)
 
     def print_entity(e):
         print("LINE on layer: %s\n" % e.dxf.layer)
         print("start point: %s\n" % e.dxf.start)
         print("end point: %s\n" % e.dxf.end)
-        print("lenght: %s\n" % str(longitude(e.dxf.start, e.dxf.end)))
+        print("lenght: %s\n" % str(get_lenght(e.dxf.start, e.dxf.end)))
 
     for e in msp:
         if e.dxftype() == "LINE":
             print_entity(e)
-    
     for e in msp.query("LINE"):
         print_entity(e)
 
