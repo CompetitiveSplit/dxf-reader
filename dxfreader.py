187c525252748f30ac801a23e4023d28e87f2b42
CompetitiveSplit <narvaez0526@gmail.com>
2023-10-22 17:19:11 -0500

    commit 2, se botiene el total (la suma de las lineas)

--- dxfreader.py
+++ dxfreader.py
@@ -6,13 +6,13 @@ try:
     my_file = ezdxf.readfile("test.dxf")
     msp = my_file.modelspace()
     total_length = 0
+
     def get_lenght(start, end):
         return (math.dist(start, end), 2)
 
     def print_entity(e):
-        print("LINE on layer: %s\n" % e.dxf.layer)
-        print("start point: %s\n" % e.dxf.start)
-        print("end point: %s\n" % e.dxf.end)
+        global total_length
+        total_length += get_lenght(e.dxf.start, e.dxf.end)[0]
         print("lenght: %s\n" % str(get_lenght(e.dxf.start, e.dxf.end)))
 
     for e in msp:
@@ -21,9 +21,12 @@ try:
     for e in msp.query("LINE"):
         print_entity(e)
 
+    print("Total length: ", total_length)
+
 except IOError:
     print(f"Not a DXF file or a generic I/O error.")
     sys.exit(1)
 except ezdxf.DXFStructureError:
     print(f"Invalid or corrupted DXF file.")
     sys.exit(2)
+
